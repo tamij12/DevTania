e3bb560da3427baae2460a4f50a2a5d3b9ab85e6
tamij12 <tamijangosrz@gmail.com>
2025-01-30 17:13:44 -0600

    framer motion installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
-import AboutMe from './components/AboutMe';
+import AboutMe from "./components/AboutMe";
+import Certifications from "./components/Certifications";
+
+import { motion } from "motion/react";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
       <Banner />
-      <AboutMe/>
-      <Projects />
-      <Skills />
-      {/* <Contact /> */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <AboutMe />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Projects />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Skills />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Certifications />
+      </motion.div>
       <Footer />
     </div>
   );
